90d2b208c24abaa0d02488ff79dbd05f59214d55
brasseld <brasseld@gmail.com>
2016-10-20 17:14:24 +0200

    Add script for migration to 1.0.0

---
+++ upgrades/1.0.0/mongodb/5-update-published-api.js
@@ -0,0 +1,68 @@
+db.apis.find().forEach(
+    function(api) {
+        db.events.find({'properties.api_id': api._id}).sort({'createdAt':-1}).limit(1).forEach(
+            function(event) {
+                if (event.type === 'PUBLISH_API') {
+
+                    var payload = JSON.parse(event.payload);
+                    var definition = JSON.parse(payload.definition);
+
+                    var dumpRequest = false;
+
+                    if (definition.proxy.http !== undefined) {
+                        dumpRequest = definition.proxy.http.configuration.dumpRequest;
+                    }
+
+                    for (var i = 0 ; i < definition.proxy.endpoints.length ; i++) {
+                        var endpoint = definition.proxy.endpoints[i];
+                        endpoint.name = 'endpoint_' + i;
+
+                        if (definition.proxy.http !== undefined) {
+                            endpoint.http = definition.proxy.http.configuration;
+
+                            if (definition.proxy.http.http_proxy) {
+                                endpoint.proxy = definition.proxy.http.http_proxy;
+                            }
+
+                            if (definition.proxy.http.ssl) {
+                                endpoint.ssl = definition.proxy.http.ssl;
+                            }
+
+                            delete endpoint.http.dumpRequest;
+                        }
+
+                        delete definition.proxy.http;
+                    }
+
+                    if (dumpRequest) {
+                        definition.proxy.dumpRequest = dumpRequest;
+                    }
+                    
+                    var paths = Object.keys(definition.paths);
+
+                    for (var i = 0 ; i < paths.length ; i++) {
+                        var path = definition.paths[paths[i]];
+                        var apiKey = -1;
+
+                        for (var j = 0 ; j < path.length ; j++) {
+                            var fields = Object.keys(path[j]);
+                            for(var k = 0 ; k < fields.length ; k++) {
+                                if (fields[k] === 'api-key') {
+                                    apiKey = j;
+                                    break;
+                                }
+                            }
+
+                        }
+
+                        if (apiKey !== -1) {
+                            path.splice(apiKey, 1);
+                        }
+                    }
+                    
+                    db.events.updateOne({ _id: event._id}, {$set: {payload: JSON.stringify(payload)}}, { upsert: true} );
+                }
+            }
+        );
+    }
+);
